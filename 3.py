b1c070f45e0165453cd9f2e90012efe348a556ed
AbsurdKookabura <paewaib4345@masseyhigh.school.nz>
2026-03-11 15:02:05 +1300

    Working game, no history

---
+++ 3.py
@@ -0,0 +1,45 @@
+# functions go here
+
+def yes_no(question):
+
+
+
+    while True:
+
+        response = input(question).lower()
+
+        # check the user says yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+           return "no"
+        else:
+            print("please enter yes / no")
+
+def instructions():
+
+
+    print("""
+*** Instructions ****
+
+Roll the dice and try to win!
+    """)
+
+    return ""
+
+
+def int_check():
+    pass
+
+# Main routine
+
+# ask the user if they want instructions (check they say yes / no)
+want_instructions = yes_no("Do you want to see the instructions ")
+
+# Display the instructions if the user wants to see them...
+if want_instructions == "yes":
+    instructions()
+
+print()
+game_goal = int_check()
+print(game_goal)
